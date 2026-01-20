b18af361692622cd8486cde8f7875af0a2d0fa17
Martseniuk Alina <martseniuk.av@gmail.com>
2026-01-20 20:00:35 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
+import stickers from "../stickers.json";
+import { StickerList } from "./components/StickerList";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <StickerList stickers={stickers} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/StickerList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class StickerList extends React.Component {
+  state = {};
+
+  render() {
+    return <h1>heloooooooooooooooooooooooooooooooooooooooo</h1>;
+  }
+}
